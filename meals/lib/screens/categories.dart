fd418d36556634b22a766b104a0a3b43f22ddfaf
Johna210 <solomonjohna21@gmail.com>
2024-03-21 00:31:54 +0300

    Meals App initial version

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(
+          title: 'Some title',
+          meals: [],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          for (final category in availableCategories)
+            CategoryItem(
+              category: category,
+              onSelectedcategory: () {
+                _selectCategory(context);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
