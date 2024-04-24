f22842becbbf1b371c5d2f1c3b5dc2197c46ce04
Johna210 <solomonjohna21@gmail.com>
2024-04-24 16:51:25 +0300

    started on the data layer

--- trivia/test/features/number_trivia/domain/usecases/get_random_number_test.dart
+++ trivia/test/features/number_trivia/domain/usecases/get_random_number_test.dart
@@ -3,6 +3,7 @@ import 'package:trivia/features/number_trivia/domain/entities/number_trivia.dart
 import 'package:trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:trivia/core/usecases/usecase.dart';
 import 'package:trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
