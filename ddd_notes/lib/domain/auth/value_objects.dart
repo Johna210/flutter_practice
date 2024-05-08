a519411c06b12482f13232a737cd5cd9cc394419
Johna210 <solomonjohna21@gmail.com>
2024-05-08 14:29:29 +0300

    Implemented domain layer for auth

---
+++ ddd_notes/lib/domain/auth/value_objects.dart
@@ -0,0 +1,29 @@
+import 'package:ddd_notes/domain/core/failures.dart';
+import 'package:ddd_notes/domain/core/value_objects.dart';
+import 'package:ddd_notes/domain/core/value_validators.dart';
+import 'package:dartz/dartz.dart';
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(validateEmailAddress(input));
+  }
+
+  const EmailAddress._(this.value);
+
+  @override
+  String toString() => 'EmailAddress(email: $value)';
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(validatePassword(input));
+  }
+
+  const Password._(this.value);
+}
---
+++ ddd_notes/lib/domain/core/value_validators.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_notes/domain/core/failures.dart';
+
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
+  const emailRegex =
+      r'^[a-zA-Z0-9.a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return Right(input);
+  } else {
+    return Left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validatePassword(String input) {
+  if (input.length >= 6) {
+    return Right(input);
+  } else {
+    return Left(ValueFailure.shortPassword(failedValue: input));
+  }
+}
